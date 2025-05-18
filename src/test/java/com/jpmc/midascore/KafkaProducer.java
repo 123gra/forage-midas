d8b9ee55f4ddbfd03283cebd1730d546829fbfb0
Tname <ttyagi.25052gmail.com>
2025-05-18 12:21:02 +0530

    completed task 1

--- src/test/java/com/jpmc/midascore/KafkaProducer.java
+++ src/test/java/com/jpmc/midascore/KafkaProducer.java
@@ -2,6 +2,7 @@ package com.jpmc.midascore;
 
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
