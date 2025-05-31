63f04dd6ffb641f2b688d1bd857288e61cc98962
Manya Gupta <gupta.manya115@gmail.com>
2025-05-31 19:28:48 +0530

    allow

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,31 +1,17 @@
 package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.foundation.Transaction;
-import com.jpmc.midascore.entity.UserRecord;
-import com.jpmc.midascore.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class TransactionListener {
-    private final UserRepository repo;
-    private final DatabaseConduit conduit;
-
-    public TransactionListener(UserRepository repo, DatabaseConduit conduit) {
-        this.repo = repo;
-        this.conduit = conduit;
-    }
+    private static final Logger logger = LoggerFactory.getLogger(TransactionListener.class);
 
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-group")
-    @Transactional
     public void handle(Transaction t) {
-        UserRecord sender = repo.findById(t.getSenderId());
-        sender.setBalance(sender.getBalance() - t.getAmount());
-        conduit.save(sender);
-
-        UserRecord recipient = repo.findById(t.getRecipientId());
-        recipient.setBalance(recipient.getBalance() + t.getAmount());
-        conduit.save(recipient);
+        logger.info("Received transaction: {}", t);
     }
 } 
