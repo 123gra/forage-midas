80c33eae8e121299987aaf9d1f12aad7358d6f5b
Henryarrovin <henry.a.s.arrovin@gmail.com>
2025-08-04 10:21:19 +0530

    Integrated H2 database to the codebase.

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,11 +1,21 @@
 package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.service.TransactionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionListener.class);
+    private final TransactionService transactionService;
+
+    public TransactionListener(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
 
     @KafkaListener(
             topics = "${kafka.topic.transaction}",
@@ -14,6 +24,11 @@ public class TransactionListener {
     )
     public void listen(Transaction transaction) {
         System.out.println(transaction);
+        try {
+            transactionService.processTransaction(transaction);
+        } catch (Exception e) {
+            logger.error("Error processing transaction: {}", e.getMessage(), e);
+        }
     }
 
 }
