7d76239e7bc7ffe59c0871539351e01991fb3450
Henryarrovin <henry.a.s.arrovin@gmail.com>
2025-08-04 12:06:33 +0530

    Updated BalanceController

--- src/main/java/com/jpmc/midascore/controller/BalanceController.java
+++ src/main/java/com/jpmc/midascore/controller/BalanceController.java
@@ -1,6 +1,5 @@
 package com.jpmc.midascore.controller;
 
-import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Balance;
 import com.jpmc.midascore.service.BalanceService;
 import org.springframework.web.bind.annotation.GetMapping;
