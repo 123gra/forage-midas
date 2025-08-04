31d37ea48bcef3f71605f9d2e0369992e591f839
Henryarrovin <henry.a.s.arrovin@gmail.com>
2025-08-04 12:01:52 +0530

    Created endpoint to get balance of user.

---
+++ src/main/java/com/jpmc/midascore/controller/BalanceController.java
@@ -0,0 +1,26 @@
+package com.jpmc.midascore.controller;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.service.BalanceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/balance")
+public class BalanceController {
+
+    private final BalanceService balanceService;
+
+    public BalanceController(BalanceService balanceService) {
+        this.balanceService = balanceService;
+    }
+
+    @GetMapping
+    public Balance getBalance(@RequestParam String userId) {
+        return balanceService.getUserBalance(userId);
+    }
+
+}
---
+++ src/main/java/com/jpmc/midascore/service/BalanceService.java
@@ -0,0 +1,9 @@
+package com.jpmc.midascore.service;
+
+import com.jpmc.midascore.foundation.Balance;
+
+public interface BalanceService {
+
+    Balance getUserBalance(String userId);
+
+}
