9492514d036a2b34d196170fe54a138e88c4df22
ketan <ketansingla7988@gmail.com>
2025-09-29 01:40:24 +0530

    Task 1 completed

--- src/test/java/com/jpmc/midascore/KafkaProducer.java
+++ src/test/java/com/jpmc/midascore/KafkaProducer.java
@@ -2,10 +2,12 @@ package com.jpmc.midascore;
 
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "spring.kafka.bootstrap-servers")
 public class KafkaProducer {
     private final String topic;
     private final KafkaTemplate<String, Transaction> kafkaTemplate;
