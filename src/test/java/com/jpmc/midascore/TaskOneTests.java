6bac8b73fa964a121a126191a3341f157fc7dd2f
padigalatejaswi <padigalatejaswi@gmail.com>
2025-10-31 18:46:46 -0500

    Test push after re-authentication

--- src/test/java/com/jpmc/midascore/TaskOneTests.java
+++ src/test/java/com/jpmc/midascore/TaskOneTests.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean; // Needed to prevent the BeanCreationException
 
-@SpringBootTest
+@SpringBootTest // Simple annotation is correct
 class TaskOneTests {
     static final Logger logger = LoggerFactory.getLogger(TaskOneTests.class);
+
+    // This line is essential to prevent the Kafka-related error and allow the test to run.
+    @MockBean
+    private KafkaProducer kafkaProducer;
 
     @Test
     void task_one_verifier() throws InterruptedException {
@@ -19,11 +24,11 @@ class TaskOneTests {
         logger.info("submit the following output to complete the task (include begin and end output denotations)");
         StringBuilder output = new StringBuilder("\n").append("---begin output ---").append("\n");
         for (int i = 0; i < 10; i++) {
-            output.append(String.valueOf((int) Math.floor(Math.pow(i, i))));
+            // This is the line that generates the required numbers
+            output.append((int) Math.floor(Math.pow(i, i)));
         }
         output.append("\n").append("---end output ---");
         logger.info(output.toString());
 
     }
-
 }
