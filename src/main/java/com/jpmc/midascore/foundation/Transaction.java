c7dca7402e3b5cde9f2f612a2711edb3717d3a92
Harshit Rana <ranaharshit0503@icloud.com>
2025-12-15 22:00:44 +0530

    fix# Please enter the commit message for your changes. Lines starting

--- src/main/java/com/jpmc/midascore/foundation/Transaction.java
+++ src/main/java/com/jpmc/midascore/foundation/Transaction.java
@@ -1,44 +1,24 @@
 package com.jpmc.midascore.foundation;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Transaction {
     private long senderId;
     private long recipientId;
     private float amount;
-
-    public Transaction() {
-    }
+    private float incentive;
 
     public Transaction(long senderId, long recipientId, float amount) {
         this.senderId = senderId;
         this.recipientId = recipientId;
         this.amount = amount;
-    }
-
-    public long getSenderId() {
-        return senderId;
-    }
-
-    public void setSenderId(long senderId) {
-        this.senderId = senderId;
-    }
-
-    public long getRecipientId() {
-        return recipientId;
-    }
-
-    public void setRecipientId(long recipientId) {
-        this.recipientId = recipientId;
-    }
-
-    public float getAmount() {
-        return amount;
-    }
-
-    public void setAmount(float amount) {
-        this.amount = amount;
+        this.incentive = 0;
     }
 
     @Override
